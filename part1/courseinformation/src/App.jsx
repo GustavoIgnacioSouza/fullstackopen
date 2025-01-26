e72654cfa2293320ea6fb611769c50e03544c75c
GustavoIgnacioSouza <125296147+GustavoIgnacioSouza@users.noreply.github.com>
2025-01-26 11:30:08 -0300

    Exercise 1.2 completed

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -6,12 +6,23 @@ const Header = ({course}) => {
   )
 }
 
-const Content = ({ part, exercises }) => {
+const Part = ({ part, exercises }) => {
   console.log (part, exercises)
   return (
     <p>{part} {exercises}</p>
   )
 }
+
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  )
+}
+
 
 const Total = ({itensToSum}) => {
   let sum = 0
@@ -36,11 +47,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total itensToSum={[exercises1, exercises2, exercises3]}/> 
       
     </div>
